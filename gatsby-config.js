637ad793de5d41a5608436f328fbe957fedc343a
szewczukk <dmitro.szewczuk@gmail.com>
2020-01-15 13:57:35 +0100

    Globl styling added

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    /*  
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -13,6 +14,7 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    */
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import styled from 'styled-components';
+import {createGlobalStyle} from 'styled-components';
 
 import Header from './header';
 
+const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-size: 1rem;
+  font-weight: 300;
+}  
+
+body {
+    margin: 0;
+  }
+`
+
 export default ({ children }) => (
   <>
+    <GlobalStyle/>
     <Header />
     {children}
   </>
