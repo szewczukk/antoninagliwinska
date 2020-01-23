75aa208f77750c8b82d00bb59f528d710b667e4d
szewczukk <dmitro.szewczuk@gmail.com>
2020-01-23 12:17:11 +0100

    Google analytics added

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,6 +49,13 @@ module.exports = {
 				],
 			},
 		},
+		{
+			resolve: `gatsby-plugin-google-analytics`,
+			options: {
+				trackingId: 'UA-147438970-2',
+				head: false,
+			},
+		},
 		// this (optional) plugin enables Progressive Web App + Offline functionality
 		// To learn more, visit: https://gatsby.dev/offline
 		// `gatsby-plugin-offline`,
